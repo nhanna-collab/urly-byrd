46f01465ebce69ae332574733d8d08f9c1e9f821
nhanna-collab <nhanna@gmail.com>
2025-11-23 13:09:41 -0800

    Add client/src/components/PageContainer.tsx

---
+++ client/src/components/PageContainer.tsx
@@ -0,0 +1,7 @@
+export default function PageContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-8">
+      {children}
+    </div>
+  );
+}
